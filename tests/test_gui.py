db15c90a2688aac2d9fd7c11fb00f9e395cf7dbe
Peter Pignatone <126121002+peter9825@users.noreply.github.com>
2025-02-24 09:59:36 -0500

    reverted changes by changing time.sleep to 1.

--- tests/test_gui.py
+++ tests/test_gui.py
@@ -91,7 +91,7 @@ class TestUserProfileInsertion(unittest.TestCase):
                 os.remove(self.db_path)
                 break
             except PermissionError:
-                time.sleep(0.5)
+                time.sleep(1)
 
     def test_save_user_profile(self):
         """Test that saving a user profile inserts the correct data into the database."""
